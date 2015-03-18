f8943d88d1cd120b4aec7897a5c166350bd0e28d
abaramomer <xjune93@gmail.com>
2015-03-18 04:01:38 +0300

    Three layers - simple asp mvc, generated db model, empty bussines

---
+++ FurnitureFabric/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FurnitureFabric.Startup))]
+namespace FurnitureFabric
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
